3505319426fdbe7b255c2595806b3b1d95adca82
Nikolai Tatianenko <nikolai.tatianenko@gmail.com>
2019-08-25 09:20:42 +0000

    feat: PHP memcached snippets - CRUD

--- snippets/memcached/php/memcached_snippets_basic.php
+++ snippets/memcached/php/memcached_snippets_basic.php
@@ -69,3 +69,51 @@ function memcached_exists($key)
     $memcached->addServer('localhost', 11211);
     return $memcached->exists($key);
 }
+
+/**
+ * Sets multiple items in Memcached.
+ *
+ * @param array $items An associative array of key-value pairs.
+ */
+function memcached_set_multi(array $items)
+{
+    $memcached = new Memcached();
+    $memcached->addServer('localhost', 11211);
+    $memcached->setMulti($items);
+}
+
+/**
+ * Gets multiple items from Memcached.
+ *
+ * @param array $keys An array of keys to retrieve.
+ * @return array An associative array of key-value pairs.
+ */
+function memcached_get_multi(array $keys)
+{
+    $memcached = new Memcached();
+    $memcached->addServer('localhost', 11211);
+    return $memcached->getMulti($keys);
+}
+
+/**
+ * Deletes a key from Memcached.
+ *
+ * @param string $key The key to delete.
+ */
+function memcached_delete($key)
+{
+    $memcached = new Memcached();
+    $memcached->addServer('localhost', 11211);
+    $memcached->delete($key);
+}
+
+/**
+ * Flushes all keys from Memcached.
+ */
+function memcached_flush()
+{
+    $memcached = new Memcached();
+    $memcached->addServer('localhost', 11211);
+    $memcached->flush();
+}
+
