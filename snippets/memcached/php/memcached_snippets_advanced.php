03cfcf2ff51747c8cb4ee369d5a393a8892efeef
Nikolai Tatianenko <nikolai.tatianenko@gmail.com>
2019-08-27 17:56:20 +0000

    feat: PHP memcached snippets - advanced - consistent hashing

--- snippets/memcached/php/memcached_snippets_advanced.php
+++ snippets/memcached/php/memcached_snippets_advanced.php
@@ -14,3 +14,22 @@ function memcached_large_value()
     $retrievedValue = $memcached->get('large_value');
     echo strlen($retrievedValue); // Output: 1048576
 }
+
+/**
+ * Using Memcached with consistent hashing for distributed caching.
+ */
+function memcached_consistent_hashing()
+{
+    $memcached = new Memcached();
+    $servers = [
+        ['memcached1.example.com', 11211, 33],
+        ['memcached2.example.com', 11211, 67],
+    ];
+    $memcached->addServers($servers);
+
+    $key = 'memcached_key';
+    $value = 'Hello, Memcached!';
+    $memcached->setByKey($servers[1][0], $key, $value);
+    $result = $memcached->getByKey($servers[1][0], $key);
+    echo $result; // Output: Hello, Memcached!
+}
