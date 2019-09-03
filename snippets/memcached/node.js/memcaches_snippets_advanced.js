96d1910b5ae305d320bab5ecb291feee7b2b060d
Nikolai Tatianenko <nikolai.tatianenko@gmail.com>
2019-09-03 06:56:20 +0000

    feat:Node.js memcached snippets - advanced - Consistent hashing

--- snippets/memcached/node.js/memcaches_snippets_advanced.js
+++ snippets/memcached/node.js/memcaches_snippets_advanced.js
@@ -22,3 +22,32 @@ function memcachedLargeValue() {
     });
   });
 }
+
+/**
+ * Using Memcached with consistent hashing for distributed caching.
+ */
+function memcachedConsistentHashing() {
+  const memcached = new Memcached();
+  const servers = [
+    { host: 'memcached1.example.com', port: 11211, weight: 33 },
+    { host: 'memcached2.example.com', port: 11211, weight: 67 },
+  ];
+  memcached.addServers(servers);
+
+  const key = 'memcached_key';
+  const value = 'Hello, Memcached!';
+  memcached.setByKey(servers[1].host, key, value, 0, (err) => {
+    if (err) {
+      console.error('Error setting value using consistent hashing in Memcached:', err);
+    }
+    memcached.getByKey(servers[1].host, key, (err, result) => {
+      if (err) {
+        console.error('Error getting value using consistent hashing from Memcached:', err);
+      } else {
+        console.log(result); // Output: Hello, Memcached!
+      }
+      memcached.end();
+    });
+  });
+}
+
